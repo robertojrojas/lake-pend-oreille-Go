21a7283aa8184a9833972aabbf272efa42222e6e
Roberto Rojas <robertojrojas@gmail.com>
2015-05-30 00:07:59 -0400

    added request helper

--- src/main/main.go
+++ src/main/main.go
@@ -2,28 +2,29 @@ package main
 
 import (
 	"fmt"
-	"models"
-	"log"
-	"os"
+//	_ "models"
+//	_ "log"
+//	_ "os"
+	"web"
 )
 
 func main() {
 
-	fmt.Printf("Hello From Lake Pend Oreille!\n")
+	//fmt.Printf("Hello From Lake Pend Oreille!\n")
 
-	lakeDatas, err := models.GetDBRecordsFor("airtemp", "2015-01-01")
-	if err != nil {
-		log.Fatal("Apolo we hear ya. Sit Tight!", err)
-		os.Exit(1)
-	}
-
-	for _, lakeData := range lakeDatas {
-		fmt.Printf("Lake data %s %s %s %s\n",
-				   lakeData.DataSource,
-			       lakeData.DateRecorded,
-			       lakeData.TimeRecorded,
-			       lakeData.RecordedValue)
-	}
+//	lakeDatas, err := models.GetDBRecordsFor("airtemp", "2015-01-01")
+//	if err != nil {
+//		log.Fatal("Apolo we hear ya. Sit Tight!", err)
+//		os.Exit(1)
+//	}
+//
+//	for _, lakeData := range lakeDatas {
+//		fmt.Printf("Lake data %s %s %s %s\n",
+//				   lakeData.DataSource,
+//			       lakeData.DateRecorded,
+//			       lakeData.TimeRecorded,
+//			       lakeData.RecordedValue)
+//	}
 
 //	sampleData := []models.LakeData{
 //		models.LakeData{"airtemp","2015_01_01","22:38:52","23.24"},
@@ -32,5 +33,15 @@ func main() {
 //		models.LakeData{"airtemp","2015_01_01","22:53:02","23.32"},
 //	}
 //	models.StoreRecords(sampleData)
+
+
+	request := &web.Request{Url: "http://lpo.dt.navy.mil/data/DM/2015/2015_01_01/Air_Temp"}
+	request.Get()
+
+	if request.IsOK() {
+		fmt.Printf("Data [%s]\n", request.ToString())
+	} else {
+		fmt.Printf("Problems getting data %s", request.Err)
+	}
 
 }
---
+++ src/web/request.go
@@ -0,0 +1,46 @@
+package web
+
+import (
+	"net/http"
+	"io/ioutil"
+	"errors"
+)
+
+type Request struct {
+	Url string
+	body []byte
+	Err error
+}
+
+func (r *Request) Get() {
+
+	resp, err := http.Get(r.Url)
+
+	if err != nil {
+		r.Err = err
+		return
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		r.Err = errors.New(resp.Status)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	r.body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		r.Err = err
+		return
+	}
+
+}
+
+func (r *Request) IsOK() (bool) {
+	return r.Err == nil
+}
+
+func (r *Request) ToString() (string) {
+	return string(r.body)
+}
+
